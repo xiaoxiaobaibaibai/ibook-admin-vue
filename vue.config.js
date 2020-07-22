537b56c28500b0ae14c986b8ee2899b592f445b6
bai <2516220191@qq.com>
2020-07-22 20:11:12 +0800

    token

--- vue.config.js
+++ vue.config.js
@@ -39,7 +39,7 @@ module.exports = {
   },
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
-    // it can be accessed in index.html to inject the correct title.
+    // it can be accessed in table.html to inject the correct title.
     name: name,
     resolve: {
       alias: {
